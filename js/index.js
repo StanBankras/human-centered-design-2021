60ecf402ec8490d3c8f7e01ffcde8bd8b712ce8e
Stan Bankras <stanbankras@gmail.com>
2021-04-22 14:54:48 +0200

    Configure emotions

--- js/index.js
+++ js/index.js
@@ -1,24 +1,5 @@
-const moreInfo = document.getElementById('more-info');
-const infoEl = document.getElementById('info');
-const colorModeBtn = document.querySelector('.mode');
-const body = document.querySelector('body');
+import { setConfigurationOptions, setSelectedEmotions } from './modules/emotion.js';
+import './modules/static.js';
 
-moreInfo.addEventListener('click', () => {
-  if(infoEl.classList.contains('show-more')) {
-    infoEl.classList.remove('show-more');
-    moreInfo.innerText = 'Lees meer over deze aflevering';
-  } else {
-    infoEl.classList.add('show-more');
-    moreInfo.innerText = 'Sluit extra informatie';
-  }
-});
-
-colorModeBtn.addEventListener('click', e => {
-  if(body.classList.contains('dark')) {
-    e.target.classList.remove('dark');
-    body.classList.remove('dark');
-  } else {
-    e.target.classList.add('dark');
-    body.classList.add('dark');
-  }
-});
+setSelectedEmotions();
+setConfigurationOptions();
---
+++ js/modules/static.js
@@ -0,0 +1,35 @@
+const moreInfo = document.getElementById('more-info');
+const infoEl = document.getElementById('info');
+const colorModeBtn = document.querySelector('.mode');
+const body = document.querySelector('body');
+
+moreInfo.addEventListener('click', () => {
+  if(infoEl.classList.contains('show-more')) {
+    infoEl.classList.remove('show-more');
+    moreInfo.innerText = 'Lees meer over deze aflevering';
+  } else {
+    infoEl.classList.add('show-more');
+    moreInfo.innerText = 'Sluit extra informatie';
+  }
+});
+
+colorModeBtn.addEventListener('click', e => {
+  if(body.classList.contains('dark')) {
+    e.target.classList.remove('dark');
+    body.classList.remove('dark');
+  } else {
+    e.target.classList.add('dark');
+    body.classList.add('dark');
+  }
+});
+
+const emotionsTopEl = document.querySelector('#emotion .top');
+
+emotionsTopEl.addEventListener('click', e => {
+  const parent = emotionsTopEl.parentElement;
+  if(parent.classList.contains('active')) {
+    parent.classList.remove('active');
+  } else {
+    parent.classList.add('active');
+  }
+});
