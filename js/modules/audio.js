d70e446c9500199207c18934f05f3b5bdd68794c
Stan Bankras <stanbankras@gmail.com>
2021-04-23 17:31:20 +0200

    Functionaliteit voice visualisatie

--- js/modules/audio.js
+++ js/modules/audio.js
@@ -7,40 +7,47 @@ const container = document.querySelector('.canvas');
 const textContainer = document.querySelector('#text');
 const waveCount = document.querySelector('#waves');
 const reader = document.querySelector('.canvas div');
+
+const speakerColors = ['#2be4a0', '#2ba0e4', '#e4892b', '#b62be4', '#e42b2b'];
 const script = [
   {
+    id: 1,
     start: 0,
     end: 24,
+    speaker: 'Person Name',
     text: 'Maybe like there\'s something about the aspect ratios on screensss, cause I feel like if you put a fixed position top bar, there\'s a certain people out there, I\'ve met them, that are like: I hate this. Like please don\'t chew into my screen realestate with your fixed topbar, but for some reason, you fix the sidebar and nobody cares you know, but maybe it chews up space that is not as useful, cause you have more horizontal space?'
   },
   {
+    id: 2,
     start: 24,
     end: 40,
+    speaker: 'Person2 Name',
     text: 'Let\'s fi... eh.. I would be curious like... where this first showed up.. what.. cause, I mean, was it the iPad? I mean, remember when like Steve Jobs like rotated the iPad for the first time, then all of a sudden mail has a sidebar, or you know, Twitter, or.. like.'
   },
   {
+    id: 3,
     start: 40,
     end: 45,
+    speaker: 'Person Name',
     text: 'oh yeah, Twitter is like this now too I isn\'t it? Yeah.'
   }
 ]
+const speakerSet = script.reduce((acc, curr) => acc.add(curr.speaker), new Set([]));
+const speakers = Array.from(speakerSet).map((speaker, i) => {
+  return {
+    speaker,
+    bgColor: speakerColors[i]
+  }
+});
+
+
 let currentText;
 let dragging = false;
 let customWidth;
 let currentSecond;
 
-drawAudio('../../assets/shoptalk-clip.mp3', 1000, 4000);
-
-
-for(let i = 0; i < 20; i++) {
-  const div = document.createElement('div');
-  div.classList.add(`sentence-${i}`);
-  div.style.position = 'absolute';
-  div.style.top = `${i * 33}px`;
-  div.style.height = '33px';
-  div.style.zIndex = -1;
-  textContainer.appendChild(div);
-}
+drawAudio('../../assets/shoptalk-clip.mp3', 500, 4000);
+drawText();
 
 audio.addEventListener('play', e => {
   playScript(e.target.currentTime);
@@ -77,17 +84,12 @@ function playScript(second) {
   const totalDuration = 45;
   const text = script.find(s => s.start <= second && s.end >= second);
   if(!text) return;
-  if(!currentText || JSON.stringify(currentText) !== JSON.stringify(text)) {
-    currentText = text;
-    appendWords(textContainer, text.text);
-    resetHighlighting();
-  }
   const textProgress = (second - text.start) / (text.end - text.start) * 100;
   const canvasWidth = canvas.width;
   let containerWidth = container.getBoundingClientRect().width;
   containerWidth = customWidth ? customWidth : containerWidth - containerWidth / 2;
 
-  drawTextProgress(textProgress, text.end - text.start);
+  drawTextProgress(textProgress, text.id);
   canvas.style.transform = `translateX(${containerWidth - second / totalDuration * canvasWidth}px)`;
 }
 
@@ -106,19 +108,55 @@ function appendWords(container, sentence) {
   });
 }
 
-function drawTextProgress(percentage, duration) {
-  const textBox = textContainer.getBoundingClientRect();
+function drawText() {
+  script.forEach(paragraph => {
+    const container = document.createElement('div');
+    container.classList.add('text-wrapper');
+    container.classList.add(`section-${paragraph.id}`);
+
+    const personTag = document.createElement('div')
+    personTag.classList.add('person-tag');
+    personTag.innerText = paragraph.speaker;
+    personTag.style.backgroundColor = speakers.find(s => s.speaker === paragraph.speaker).bgColor;
+
+    const text = document.createElement('div');
+    const textWrapper = document.createElement('div');
+    text.classList.add('text');
+    text.appendChild(textWrapper);
+    textWrapper.classList.add('wrapper');
+    appendWords(textWrapper, paragraph.text);
+
+    container.appendChild(personTag);
+    container.appendChild(text);
+    textContainer.appendChild(container);
+
+    for(let i = 0; i < 40; i++) {
+      const div = document.createElement('div');
+      div.classList.add(`sentence-${i}-${paragraph.id}`);
+      div.style.position = 'absolute';
+      div.style.top = `${i * 33 + 15}px`;
+      div.style.height = '33px';
+      div.style.zIndex = -1;
+      text.appendChild(div);
+    } 
+  });
+}
+
+function drawTextProgress(percentage, scriptIndex) {
+  const textBox = document.querySelector(`.section-${scriptIndex} .text .wrapper`);
+  const textDimensions = textBox.getBoundingClientRect();
   const lines = textBox.height / 33;
-  const children = Array.from(textContainer.children);
+  const children = Array.from(textBox.children);
   const spans = children.filter(child => child.tagName === 'SPAN');
   const totalWords = spans.length;
+
 
   const wordsPerLine = [];
   spans.forEach(span => {
     const box = span.getBoundingClientRect();
     const offset = 33;
     for(let i = 0; i < 100; i++) {
-      if(box.top - (20 + i * offset) < textBox.top) {
+      if(box.top - (20 + i * offset) < textDimensions.top) {
         if(wordsPerLine[i]) {
           wordsPerLine[i].push(span);
           break;
@@ -133,18 +171,18 @@ function drawTextProgress(percentage, duration) {
   const wordIndex = Math.floor(totalWords * (percentage / 100))
 
   wordsPerLine.forEach((line, i) => {
-    const sentence = document.querySelector(`.sentence-${i}`);
+    const sentence = document.querySelector(`.sentence-${i}-${scriptIndex}`);
     let wordsBefore = 0;
     
     for(let j = 0; j < i; j++) {
-      wordsBefore += wordsPerLine[j].length;
+      wordsBefore += (wordsPerLine[j] || []).length;
     }
     
     if(wordIndex > line.length + wordsBefore) {
-      setHighlighting(sentence, textBox.width);
+      setHighlighting(sentence, textDimensions.width);
     } else if(wordIndex < line.length + wordsBefore && wordIndex > wordsBefore) {
       const index = wordIndex - wordsBefore;
-      const width = line[index].getBoundingClientRect().left - textBox.left;
+      const width = line[index].getBoundingClientRect().left - textDimensions.left;
       setHighlighting(sentence, width);
     } else {
       setHighlighting(sentence, 0);
