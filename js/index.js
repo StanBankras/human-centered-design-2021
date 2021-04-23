8f2d1409a63983c6301ae8c2729802cc2a991378
Stan Bankras <stanbankras@gmail.com>
2021-04-23 15:06:25 +0200

    Implementeer audio visualizer in de app

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import { setConfigurationOptions, setSelectedEmotions } from './modules/emotion.js';
 import './modules/static.js';
+import './modules/audio.js';
 
 setSelectedEmotions();
 setConfigurationOptions();
